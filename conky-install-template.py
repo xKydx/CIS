4e9031e035125eb7788fe01067f1f01a80f612d3
D Inc <xKydx@users.noreply.github.com>
2016-03-31 10:25:58 -0400

    Added template, ref-material and other

---
+++ conky-install-template.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import os
+import sys
+from sys import argv
+
+usrnpt = argv
+
+prompt = ".:|"
+
+os.system('clear')
+print "Name-of-Conky-to-be-installed-here"# Name of Conky
+print("\n")
+print("\n")
+print ("Install Conky file now?")
+usrnpt = raw_input(prompt)
+if 'y' in usrnpt:
+	os.system("sudo cp /NAME/OF/DIRECTORY /etc/conky/conky.conf")# EG: sudo cp ~/Downloads/conkyrc /etc/conky/conky.conf
+else:
+	print ("Not Installed")
+print("\n")
+print("\n")
+
+## Uncomment if your installing a font with your conky.
+## No need if your not.
+
+#print ("Install font now?")
+#usrnpt = raw_input(prompt)
+#if 'y' in usrnpt:
+#	os.system('sudo cp FB.ttf /etc/fonts/FB.ttf')
+#else:
+#	print ("Not Installed")
+#print("\n")
+#print("\n")
+print ("Name-Of-Conky install script complete")# Name of conky
+print("\n")
+print ("You can start conky by typing 'conky' in a terminal window.")
+print("\n")
